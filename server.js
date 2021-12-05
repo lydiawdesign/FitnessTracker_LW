ad473c1829891cdabbeebcc65cd85b8e9d0023ee
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-05 16:09:02 -0500

    deploying

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workoutDb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // require route
 app.use(require("./routes/apiRoute"));
